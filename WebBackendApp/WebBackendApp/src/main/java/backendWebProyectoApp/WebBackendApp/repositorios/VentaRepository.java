d3de9b95012f08799ed522150b72178cd2eb091c
jkevinaparicio <129012266+jkevinaparicio@users.noreply.github.com>
2026-09-12 13:47:39 -0500

    Refactor VentaRepository to support pagination

--- WebBackendApp/WebBackendApp/src/main/java/backendWebProyectoApp/WebBackendApp/repositorios/VentaRepository.java
+++ WebBackendApp/WebBackendApp/src/main/java/backendWebProyectoApp/WebBackendApp/repositorios/VentaRepository.java
@@ -3,7 +3,8 @@ package backendWebProyectoApp.WebBackendApp.repositorios;
 import backendWebProyectoApp.WebBackendApp.entidades.Usuario;
 import backendWebProyectoApp.WebBackendApp.entidades.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 
 import java.time.LocalDate;
@@ -11,19 +12,16 @@ import java.util.List;
 
 public interface VentaRepository extends JpaRepository<Venta, Integer> {
 
-    List<Venta> findByUsuarioOrderByFechaDesc(Usuario usuario);
-
-    List<Venta> findByUsuario_NombreOrderByFechaDesc(String nombre);
-
-    List<Venta> findBySede_IdOrderByFechaDesc(Integer idSede);
-
     List<Venta> findAllByOrderByFechaDesc();
-
+    List<Venta> findByUsuario_NombreOrderByFechaDesc(String nombre);
+    List<Venta> findBySede_IdOrderByFechaDesc(Integer idSede);
+    List<Venta> findByUsuarioOrderByFechaDesc(Usuario usuario);
     List<Venta> findByFechaNegocioOrderByFechaDesc(LocalDate fechaNegocio);
-
     List<Venta> findBySede_IdAndFechaNegocioOrderByFechaDesc(Integer idSede, LocalDate fechaNegocio);
 
-
-
-
+    Page<Venta> findAllByOrderByFechaDesc(Pageable pageable);
+    Page<Venta> findByUsuario_NombreOrderByFechaDesc(String nombre, Pageable pageable);
+    Page<Venta> findBySede_IdOrderByFechaDesc(Integer idSede, Pageable pageable);
+    Page<Venta> findByFechaNegocioOrderByFechaDesc(LocalDate fechaNegocio, Pageable pageable);
+    Page<Venta> findBySede_IdAndFechaNegocioOrderByFechaDesc(Integer idSede, LocalDate fechaNegocio, Pageable pageable);
 }
