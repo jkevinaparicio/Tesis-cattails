5ea55ec1f305001a896734f90c9a5169c640f6b5
jkevinaparicio <129012266+jkevinaparicio@users.noreply.github.com>
2026-09-17 15:05:21 -0500

    Enhance WebSocketConfig with session handshake support
    Added HttpSessionHandshakeInterceptor for WebSocket support.

--- WebBackendApp/WebBackendApp/src/main/java/backendWebProyectoApp/WebBackendApp/config/WebSocketConfig.java
+++ WebBackendApp/WebBackendApp/src/main/java/backendWebProyectoApp/WebBackendApp/config/WebSocketConfig.java
@@ -3,6 +3,7 @@ package backendWebProyectoApp.WebBackendApp.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
